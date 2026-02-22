6e0f72c0531396fd32d4295c0df0814347e78d20
Kush Gupta <kushguptacse@gmail.com>
2026-02-22 10:37:17 +0000

    default value function

---
+++ functions.py
@@ -0,0 +1,7 @@
+def meth(p1,p2,p3=0,p4=10):
+    print(p1,p2,p3,p4)
+
+meth(1,2)
+
+# def meth(p1,p2,p3=0,p4=10,p5): # invalid, non default argument cannot come after default argument.
+#     print(p1,p2,p3,p4,p5)
