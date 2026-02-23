012dd6db253b7a46c5a5f9b2f15801c2c160dfbe
Kush Gupta <kushguptacse@gmail.com>
2026-02-23 10:41:02 +0000

    added mutable default arg example

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+
 def meth(p1,p2,p3=0,p4=10):
     print(p1,p2,p3,p4)
 
@@ -5,3 +6,10 @@ meth(1,2)
 
 # def meth(p1,p2,p3=0,p4=10,p5): # invalid, non default argument cannot come after default argument.
 #     print(p1,p2,p3,p4,p5)
+
+def test(val, my_list=[]):
+    my_list.append(val)
+    print(my_list)
+
+test("k") # ['k']
+test("l") #['k', 'l']
