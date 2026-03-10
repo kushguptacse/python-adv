28f51cb222d7f17893e4e53f45a06deb4b22727d
Kush Gupta <kushguptacse@gmail.com>
2026-03-10 07:15:29 +0000

    added example of class method

---
+++ oops/class_method.py
@@ -0,0 +1,15 @@
+class Example:
+    class_var = 4
+    def __init__(self):
+        print("inside init")
+        self.ins_var = "kk"
+
+    @classmethod
+    def class_method(cls):
+        print(cls.class_var)
+        #print(cls.ins_var) not available
+
+Example.class_method() #4
+obj = Example() # inside init
+print(obj.ins_var) #kk
+obj.class_method() #4
