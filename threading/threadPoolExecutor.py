ad46a13fe1056608c76f2779a5dd1d1c8c6eaf56
Kush Gupta <kushguptacse@gmail.com>
2026-03-19 04:40:29 +0000

    refactoring thread

--- threading/threadPoolExecutor.py
+++
@@ -1,7 +0,0 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
-
-with ThreadPoolExecutor(max_workers=10) as executor:
-    futures = [executor.submit(lambda x: x**2, i) for i in range(1, 11)]
-
-    for future in as_completed(futures):
-        print(future.result())
